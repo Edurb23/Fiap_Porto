0f12d6358134ff90a9ac983603658aaef2f412b0
ppedrobg <rm551061@fiap.com.br>
2023-09-09 17:24:58 -0300

    Finalizando a pagina servicos

--- src/routes/Servicos/index.jsx
+++ src/routes/Servicos/index.jsx
@@ -10,9 +10,12 @@ const Servicos = () => {
            <section className="conteudo">
 
             <h1>Serviços</h1>
+            <div className="texto-descritivo">
             <p >
                 Nosso serviço consiste em criar um ChatBot inteligente e de fácil entendimento, esse Chatbot ajudará na coleta de informações que serão fornecidas pelo cliente. Com essas informações será possível localizar o veículo do cliente e solicitar o melhor modal para a solução do problema.
             </p>
+
+            </div>
 
             <img className="imagemchat1" src="/img/chatbot1.png" alt="Imagem de um exemplo de chatbot" />
             <img className="imagemchat1" src="/img/chatbot2.png" alt="Imagem de um exemplo de chatbot" />
@@ -21,6 +24,5 @@ const Servicos = () => {
         </body>
     )
 }
-
 
 export default Servicos
