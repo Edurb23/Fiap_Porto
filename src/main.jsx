c9b419ceefee2e78c26a36ec39648acae8893a2d
ppedrobg <rm551061@fiap.com.br>
2023-09-08 22:12:24 -0300

    Criando os paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRoute, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home'
+import Servicos from './routes/Servicos'
+import Integrantes from './routes/Integrantes'
+import Error from './routes/Error'
+
+const router = createBrowserRoute([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        
+      }
+    ]
+  }
+]
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
