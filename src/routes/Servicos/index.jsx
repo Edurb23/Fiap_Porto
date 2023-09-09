8b23a001c1e9ac797716dc2b5bfbefe381bc0db3
ppedrobg <rm551061@fiap.com.br>
2023-09-08 22:35:31 -0300

    adicionando conteudo no serviços

--- src/routes/Servicos/index.jsx
+++ src/routes/Servicos/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Cabecalho from "../../components/Cabecalho";
+import './servicos.css'
 
 const Servicos = () => {
-    return(
+    return (
         <body>
             <Cabecalho></Cabecalho>
+           <section className="conteudo">
+
+            <h1>Serviços</h1>
+            <p >
+                Nosso serviço consiste em criar um ChatBot inteligente e de fácil entendimento, esse Chatbot ajudará na coleta de informações que serão fornecidas pelo cliente. Com essas informações será possível localizar o veículo do cliente e solicitar o melhor modal para a solução do problema.
+            </p>
+           </section>
         </body>
     )
 }
