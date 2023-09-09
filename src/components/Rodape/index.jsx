4cc194010028a613e9a85c82ebb088860a146249
PedroFigas <pedh.foliveira@gmail.com>
2023-09-08 21:51:42 -0300

    criando componente rodape

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+
+
+
+const Rodape = () => {
+    return(
+        <section>
+            <ul>
+                <img src="" alt="Logo da porto" />
+                <li>
+                    <a>
+                        Sobre nos
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Serviços
+                    </a>
+                </li>
+                <li>
+                    <a>
+                       Fale conosco:
+                    </a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a>
+                        Email: autocarsevicos@fiap.com.br
+                    </a>
+                </li>
+                <li>
+                    <a href="linkedin.com" target="_blank">
+                       <img src="" alt="logo linkidn" />
+                    </a>
+                </li>
+                <li>
+                    <a href="facebook.com" target="_blank"> 
+                        <img src="" alt="logo Facebook" />
+                    </a>
+                </li>
+                <li>
+                    <a href="X.com" target="_blank"> 
+                        <img src="X" alt="Logo do Twitter" />
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
