f3e4913274ceeec8d76ba848398f1d0519da5180
PedroFigas <pedh.foliveira@gmail.com>
2023-09-08 21:26:21 -0300

    Criando a pagina Integrantes

---
+++ src/routes/Integrantes/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Cabecalho from "../../components/Cabecalho"
+
+const Integrantes = () => {
+    return(
+        <body>
+            <Cabecalho></Cabecalho>
+
+        </body>
+    )
+}
+
+export default Integrantes 
