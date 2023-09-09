ea5098eb83f1a58162279c9682e5063feb5ed9c2
ppedrobg <rm551061@fiap.com.br>
2023-09-08 22:55:23 -0300

    fazendo css do serviços

--- src/routes/Servicos/index.jsx
+++ src/routes/Servicos/index.jsx
@@ -13,8 +13,8 @@ const Servicos = () => {
                 Nosso serviço consiste em criar um ChatBot inteligente e de fácil entendimento, esse Chatbot ajudará na coleta de informações que serão fornecidas pelo cliente. Com essas informações será possível localizar o veículo do cliente e solicitar o melhor modal para a solução do problema.
             </p>
 
-            <img src="/img/chatbot1.png" alt="Imagem de um exemplo de chatbot" />
-            <img src="/img/chatbot2.png" alt="Imagem de um exemplo de chatbot" />
+            <img className="imagemchat1" src="/img/chatbot1.png" alt="Imagem de um exemplo de chatbot" />
+            <img className="imagemchat1" src="/img/chatbot2.png" alt="Imagem de um exemplo de chatbot" />
            </section>
         </body>
     )
