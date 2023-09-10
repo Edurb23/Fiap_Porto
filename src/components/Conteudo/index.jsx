78ddd9edd55cc09cfb24b1bab579707a19cb3eab
Edurb23 <rm551987@fiap.com>
2023-09-10 19:56:10 -0300

    fazendo a pagina home

---
+++ src/components/Conteudo/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./conteudo.css"
+
+const Conteudo = () => {
+    return(
+        <>
+        <section className="inicio">
+        <h1>Somos a empresa jarvis e estamos aqui para ajudar com seu problema !
+            <p>A Porto é uma holding brasileira fundada em 1945. A atuação da empresa se baseia em três verticais de negócios: Porto Seguros, Porto Saúde e Porto Seguro Bank.</p>
+            <button>Site Porto</button>
+        </h1>
+        
+       <img src="/img/Guincho_P-2-1-1.png" alt="" />
+    
+        </section>
+
+       
+
+       
+        </>
+    )
+}
+
+
+export default Conteudo
--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import "./home.css"
 import Cabecalho from "../../components/Cabecalho"
+import Conteudo  from "../../components/Conteudo"
 import Rodape from "../../components/Rodape"
 
 const Home = () => {
     return(
         <body>
           <Cabecalho></Cabecalho> 
-          
+           <Conteudo></Conteudo>
         </body>
     )
-}
+}  
 
 export default Home
