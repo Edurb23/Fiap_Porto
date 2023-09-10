94b1766c61ed71fda2d1886e4cac3c9dcd6d5177
Silvio009 <rm550821@fiap.com.br>
2023-09-10 17:16:23 -0300

    Criando os cards da pagina integrantes

---
+++ src/components/Colaborador/index.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import './colaborador.css'
+
+
+const Colaborador = () => {
+    return(
+        <div className="colaborador">
+            <div className="cabecalho">
+            <h4>Silvio Luiz Aranha Junior</h4>
+            </div>
+            <div className="rodape">
+            <h4>teste</h4>
+            <h5>teste</h5>
+            </div>
+
+        </div>
+    )
+}
+
+export default Colaborador
--- src/routes/Integrantes/index.jsx
+++ src/routes/Integrantes/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Cabecalho from "../../components/Cabecalho"
+import Colaborador from "../../components/Colaborador";
 
 const Integrantes = () => {
     return(
         <body>
             <Cabecalho></Cabecalho>
             <h1>Integrantes</h1>
+            <Colaborador></Colaborador>
 
         </body>
     )
