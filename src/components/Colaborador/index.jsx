9c1b4d69545fcba1eb503fa1b9fdc7b93bf35292
Silvio009 <rm550821@fiap.com.br>
2023-09-10 17:52:32 -0300

    Card finalizado

--- src/components/Colaborador/index.jsx
+++ src/components/Colaborador/index.jsx
@@ -2,15 +2,17 @@ import React from "react"
 import './colaborador.css'
 
 
-const Colaborador = () => {
+const Colaborador = ({nome, descricao, redesocial, rm}) => {
     return(
         <div className="colaborador">
             <div className="cabecalho">
             <h4>Silvio Luiz Aranha Junior</h4>
+            <h5>Aluno na fiap  rm550821</h5>
+            <img src="/img/icons8-linkedin.svg" alt="Linkidin logo" />
             </div>
             <div className="rodape">
-            <h4>teste</h4>
-            <h5>teste</h5>
+            
+            <h5>olá sou o silvio Luiz, tenho 18 anos sou apaixonado por programação e estou no primeiro semestre da FIAP e quero aprender o maximo possivel</h5>
             </div>
 
         </div>
